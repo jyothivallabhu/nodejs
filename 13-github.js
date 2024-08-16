08d23fe627555bacdf41e9a17bba4291ed1db230
jyothivallabhu <v.jyothi55@gmail.com>
2024-08-16 19:55:46 +0530

    commit

--- 13-github.js
+++ 13-github.js
@@ -1,5 +1,13 @@
 // create repo in github
 // create .gitignore file in project folder and mention the files or folder no need(/node_modules etc..) to move to git
 
-//now initilize git 
+//now initilize git
 //git add .
+//git commit -m "first commit"
+// now we need to copy those 3 commands from git page
+// git remote add origin https://github.com/jyothivallabhu/nodejs.git
+// git branch -M main
+// git push -u origin main
+
+
+//for using the same application from git just clone the repo and if we had package.json file  just simply run npm install in terminal this will check for dependencies if not there it will install the dependencies which are in package.json file
